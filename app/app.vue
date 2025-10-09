64eeaefe4b774dbed563279261f37438ec5da60d
David Carter <dcarter@kensiemae.com>
2025-10-09 14:37:17 -0600

    Tailwind done

--- app/app.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <NuxtRouteAnnouncer />
-    HELLO MAIN
-  </div>
-</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
   modules: ["@nuxt/eslint"],
   eslint: {
     config: {
       standalone: false,
     },
   },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  srcDir: ".",
 });
