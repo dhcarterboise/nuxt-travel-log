4d33c542f917ed4549991c83d5672978184695b5
David Carter <dcarter@kensiemae.com>
2025-10-10 09:05:24 -0600

    Landing Page

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-	<div>
-		<h1 class="text-3xl font-bold underline">
-			Hello world!
-		</h1>
-		<button class="btn btn-active btn-secondary">
-			View Map <icon name="tabler:adjustments-alt" />
-		</button>
-	</div>
+	<NuxtLayout>
+		<NuxtPage />
+	</NuxtLayout>
 </template>
